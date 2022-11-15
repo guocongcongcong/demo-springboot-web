493cd0e41a72eae070cbf8f72252e6663f842ad0
Chaos <sichao753@163.com>
2022-11-15 10:59:35 +0800

    chang request to GetMapping

--- src/main/java/com/demo/controller/DemoController.java
+++ src/main/java/com/demo/controller/DemoController.java
@@ -2,7 +2,7 @@ package com.demo.controller;
 
 import com.demo.service.DemoService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,7 +19,7 @@ public class DemoController {
     @Autowired
     private DemoService demoService;
 
-    @PostMapping("/query")
+    @GetMapping("/query")
     public String query() {
       return demoService.query();
     }
